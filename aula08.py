4af23e87a99db11645e250029b669e9b195121c2
Vinícius C. Miranda <vinicardmiranda@gmail.com>
2025-01-16 20:19:09 -0300

    classes

---
+++ aula08.py
@@ -0,0 +1,7 @@
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('Raiz de {} é igual a {:.2f}'.format(num,raiz))
+
+import emoji
+print(emoji.emojize("Hello World! :globe_showing_americas:"))
